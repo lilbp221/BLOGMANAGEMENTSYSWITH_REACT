7220f2dd9612e37a9b619ec7ef0cc210d78b6462
Binod Pokhrel <lilbp221@gmail.cok>
2024-07-09 19:24:19 +0545

    updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -31,7 +31,7 @@ dispatch(setToken(null))
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvT9uDJ9kV7C3zO6iOt95e24Dtt3QMSncmqLLsuv4KWpeC_Gx6C0AZZpyHQWps4h-Psq8&usqp=CAU" className="h-8" alt="Flowbite Logo" />
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvT9uDJ9kV7C3zO6iOt95e24Dtt3QMSncmqLLsuv4KWpeC_Gx6C0AZZpyHQWps4h-Psq8&usqp=CAU" className="h-8" alt="Blogbook logo" />
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Blogbook</span>
   </Link>
   <div className="flex md:order-2">
@@ -55,26 +55,27 @@ dispatch(setToken(null))
        
        (
         <>
-       
-       <li>
+        <li>
          <Link to="/register" className="block py-2 px-3 my-1 text-white bg-blue-700 rounded md:bg-blue-700  md:text-white">Register</Link>
        </li>
        <li>
          <Link to="/login" className="block py-2 px-3 my-1 text-white bg-blue-700 rounded md:bg-blue-700  md:text-white">Login</Link>
        </li>
+            
+       
        </>
         )
        
        }
       </ul>
-    <button data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
+    {/* <button data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
-    </button>
+    </button> */}
   </div>
-    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
+    {/* <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
       <div className="relative mt-3 md:hidden">
         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
           <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -99,7 +100,7 @@ dispatch(setToken(null))
       <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
     </div>
     
-    </div>
+    </div> */}
   </div>
 </nav>
 
--- src/pages/auth/components/form/Form.jsx
+++ src/pages/auth/components/form/Form.jsx
@@ -30,35 +30,35 @@ const Form = ({type,onSubmit,user}) => {
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
             <div className="max-w-md mx-auto">
                 <div>
-                    <h1 className="text-2xl font-semibold">
-                        { type === 'Login' ? 'Login here to continue...' : 'Register here to continue...' }
+                    <h1 className="text-2xl font-semibold font-mono text-center">
+                        { type === 'Login' ? 'LOGIN' : 'REGISTER' }
                     </h1>
                 </div>
                
                 <br/>
-               {type==='Login' && (<h1>Hello, {user?.username}</h1>)} 
+               {type==='Login' && (<h1 className="text-md font-mono ">Hello {user?.username || "User !!"}</h1>)} 
                
          <form onSubmit={handleSubmit}>
          <div className="divide-y divide-gray-200">
                     <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                         <div className="relative">
-                            <input autoComplete="off" id="email" name="email" type="email" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" onChange={handleChange} required />
-                            <label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm" >Email Address</label>
+                            <input autoComplete="off" id="email" name="email" type="email" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 font-mono " placeholder="Email address" onChange={handleChange} required />
+                            <label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm font-mono " >Email Address</label>
                         </div>
                    {
                     type === 'Register' && (
                         <div className="relative">
-                        <input autoComplete="off" id="username" name="username" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="username address" onChange={handleChange} required />
-                        <label htmlFor="username" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Username</label>
+                        <input autoComplete="off" id="username" name="username" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 font-mono " placeholder="username address" onChange={handleChange} required />
+                        <label htmlFor="username" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm font-mono ">Username</label>
                     </div>
                     )
                    }
                         <div className="relative">
-                            <input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" onChange={handleChange} required />
-                            <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                            <input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" onChange={handleChange} required font-mono />
+                            <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm font-mono ">Password</label>
                         </div>
                         <div className="relative">
-                            <button className="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
+                            <button className="bg-blue-500 text-white rounded-md px-2 py-1 font-mono ">Submit</button>
                         </div>
                     </div>
                 </div>
@@ -67,14 +67,14 @@ const Form = ({type,onSubmit,user}) => {
 
                         //use <> </> tag if more than one element in ternanry operator condition?True:False
                         <>
-                        <span className="text-black" >Already Registered? </span>
-                   <Link to='/login' style={{color:'blue'}} > Go to login</Link>
+                        <span className="text-black font-mono " >Already Registered? </span>
+                   <Link to='/login' style={{color:'blue'}} className='font-mono '> Go to login</Link>
 
                         </>
                     ) : (
                         <> 
-                        <span className="text-black" >Not Registered? </span> 
-                        <Link to='/register' style={{color:'blue'}} > Go to register</Link>
+                        <span className="text-black font-mono " >Not Registered? </span> 
+                        <Link to='/register' style={{color:'blue'}}  className='font-mono '> Go to register</Link>
 
                         </>
 
